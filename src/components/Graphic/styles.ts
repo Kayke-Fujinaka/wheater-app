0799be7bd4d879eff5b27b237beec4f2a44f1f20
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-07-20 16:26:49 -0300

    style: graphic component

--- src/components/Graphic/styles.ts
+++ src/components/Graphic/styles.ts
@@ -4,8 +4,8 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 312px;
-  height: 144px;
+  width: 500px;
+  height: 250px;
   font-size: var(--big-font-size);
   font-weight: var(--bold);
   color: var(--text-black-color);
