361bd8c462399bac496cd1a51edb4ff1bfe9bb95
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-07-20 16:18:22 -0300

    feat: graphic component

---
+++ src/components/Graphic/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 144px;
+  font-size: var(--big-font-size);
+  font-weight: var(--bold);
+  color: var(--text-black-color);
+  background: var(--text-grey-color);
+`
