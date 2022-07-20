b801b2021a31faec54fb1e033c76bb34e34f6b49
Ismael <ismaelgomesdasilva003@gmail.com>
2022-07-20 17:36:56 -0300

    feat: description day

---
+++ src/components/DescriptionDay/index.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+export const DescriptionDay = () => (
+  <div>
+    <h3>15:40, terça-feira, 19 de junho, 2022</h3>
+    <div>
+      <Image src="/images/overcast-clouds.svg" width="300" height="300" />
+      <h1>18°C</h1>
+    </div>
+    <h1>Nublado</h1>
+  </div>
+)
