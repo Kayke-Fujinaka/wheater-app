c7469ce165b828270bb32891c2a1439481799d13
Ismael <ismaelgomesdasilva003@gmail.com>
2022-07-20 18:06:21 -0300

    feat: complete structuring of the description

--- src/components/DescriptionDay/index.tsx
+++ src/components/DescriptionDay/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-export const DescriptionDay = () => (
+export const DescriptionDay: React.FC = () => (
   <div>
     <h3>15:40, terça-feira, 19 de junho, 2022</h3>
     <div>
@@ -7,5 +7,17 @@ export const DescriptionDay = () => (
       <h1>18°C</h1>
     </div>
     <h1>Nublado</h1>
+    <div>
+      <table>
+        <tr>
+          <td>Umidade</td>
+          <td>Velocidade do vento</td>
+        </tr>
+        <tr>
+          <td>45%</td>
+          <td>20km/h</td>
+        </tr>
+      </table>
+    </div>
   </div>
 )
