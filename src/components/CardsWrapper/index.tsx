954fb1baa0c080ab6419a942e5216b73cf54123e
Ismael <ismaelgomesdasilva003@gmail.com>
2022-07-25 15:39:15 -0300

    feat: format new data

--- src/components/CardsWrapper/index.tsx
+++ src/components/CardsWrapper/index.tsx
@@ -9,13 +9,25 @@ interface cardsWrapperProps {
 
 export const CardsWrapper = ({ cards }: cardsWrapperProps) => {
   const { changeIndex, cardActive } = useContext(WeatherContext)
+  function formatDate(date: string) {
+    const [weekday, month, day] = new Date(date)
+      .toLocaleDateString('pt-br', {
+        month: 'long',
+        day: 'numeric'
+      })
+      .split(' ')
+    const abbreviatedDayOfTheWeek = `${weekday.substring(0, 3)} `
+    const formattedDate = [abbreviatedDayOfTheWeek, day]
+
+    return formattedDate.join('de ')
+  }
   return (
     <>
       <Container>
         {cards.map((card, index) => {
           return (
             <DayCard
-              day={card.day}
+              day={formatDate(card.day)}
               key={index}
               humidity={card.humidity}
               className={cardActive === index ? 'default active' : 'default'}
