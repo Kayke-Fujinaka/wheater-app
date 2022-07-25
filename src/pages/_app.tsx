ca66bd4c29f1769219c39f36f7c9bf8dc5001d9d
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-07-25 16:58:54 -0300

    feat: loading screen component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,28 @@
+/* eslint-disable prettier/prettier */
 import React from 'react'
 import { AppProps } from 'next/app'
+import { LoadingScreen } from '../components/Loading'
 import { GlobalStyle } from '../styles/global'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const [loading, setLoading] = React.useState(false)
+
+  React.useEffect(() => {
+    setTimeout(() => setLoading(true), 5000)
+  }, [])
+
   return (
     <>
-      <Component {...pageProps} />
       <GlobalStyle />
+      {loading
+        ? (
+        <React.Fragment>
+          <Component {...pageProps} />
+        </React.Fragment>
+          )
+        : (
+        <LoadingScreen />
+          )}
     </>
   )
 }
