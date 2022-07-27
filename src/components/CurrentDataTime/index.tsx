5c4e9892b566c3076cced477db618e592fdb1483
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2022-07-27 11:39:05 -0300

    feat: realtime in h3

---
+++ src/components/CurrentDataTime/index.tsx
@@ -0,0 +1,31 @@
+import { RealTime } from './styles'
+export function CurrentDataTime() {
+  const data = new Date()
+  const day = [
+    'Domingo',
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado'
+  ][data.getDay()]
+  const date = data.getDate()
+  const month = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+  ][data.getMonth()]
+  const year = data.getFullYear()
+  const timer = new Date().toLocaleTimeString().slice(0, 5)
+  return <RealTime>{`${timer}, ${day}, ${date} de ${month}, ${year}`}</RealTime>
+}
